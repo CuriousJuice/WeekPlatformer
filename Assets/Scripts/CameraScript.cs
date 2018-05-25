062b9de20ed8c9a3b2203f7365d6eef43806bb3e
Jason Li <JasonLi3125@gmail.com>
2018-05-25 18:52:39 -0400

    added basic scrolling

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
+    private GameObject player;
+    private Vector3 distance;
+    private float distanceOffset;
 
 	// Use this for initialization
 	void Start () {
+        //Find camera distance
+        distance = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0));
+        distanceOffset = distance.x * -0.75F;
+
         Texture2D playerTexture = Resources.Load("square") as Texture2D;
         Sprite playerSprite = Sprite.Create(playerTexture, new Rect(new Vector2(0,0), new Vector2(100, 100)), new Vector2(0, 0));
-        GameObject playerObject = new GameObject("Player");
-        SpriteRenderer playerRenderer = playerObject.AddComponent<SpriteRenderer>();
-        playerObject.AddComponent<Player>();
+        player = new GameObject("Player");
+        SpriteRenderer playerRenderer = player.AddComponent<SpriteRenderer>();
+        player.AddComponent<Player>();
         playerRenderer.sprite = playerSprite;
 
-        playerObject.transform.position = new Vector3(0,0,-15);
+        //Initial camera position
+        gameObject.transform.position = new Vector3(0,0,-15);
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        if (player.transform.position.x > gameObject.transform.position.x)
+        {
+            gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y,
+                gameObject.transform.position.z);
+        }
+        else if (player.transform.position.x < gameObject.transform.position.x - distanceOffset)
+        {
+            gameObject.transform.position = new Vector3(player.transform.position.x + distanceOffset,
+                gameObject.transform.position.y, gameObject.transform.position.z);
+        }
 	}
 }
