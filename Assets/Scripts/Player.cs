8c11d4f51244bc21b7ccddb67815113a0a9898e9
Jason Li <JasonLi3125@gmail.com>
2018-05-25 15:20:41 -0400

    Added functionality when pressing left and right at the same time

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,26 +23,40 @@ public class Player : Character {
         Vector2 movementThisFrame = new Vector2(0, 0);
         Vector2 currentPosition = gameObject.transform.position;
 
-        if (Input.GetKey(KeyCode.RightArrow) && velocity.x < maxVelocity.x) {
-            if (velocity.x < 0) {
-                velocity *= 0.6F;
-            }
-            velocity += accelerationX;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) && -velocity.x < maxVelocity.x) {
-            if (velocity.x > 0) {
-                velocity *= 0.6F;
-            }
-            velocity -= accelerationX;
-        }
-        if(!(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))) {
-            if (System.Math.Abs(velocity.x) < 0.1F) {
+        //If neither right/left pressed or both
+        if (!(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+            || Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (System.Math.Abs(velocity.x) < 0.1F)
+            {
                 velocity.x = 0;
             }
-            else {
+            else
+            {
                 velocity *= 0.9F;
             }
         }
+        // If only one of right / left pressed
+        else
+        {
+            if (Input.GetKey(KeyCode.RightArrow) && velocity.x < maxVelocity.x)
+            {
+                if (velocity.x < 0)
+                {
+                    velocity *= 0.6F;
+                }
+                velocity += accelerationX;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) && -velocity.x < maxVelocity.x)
+            {
+                if (velocity.x > 0)
+                {
+                    velocity *= 0.6F;
+                }
+                velocity -= accelerationX;
+            }
+        }
+        
         if (Input.GetKey(KeyCode.UpArrow))
         {
             velocity.y += 0.2F;
