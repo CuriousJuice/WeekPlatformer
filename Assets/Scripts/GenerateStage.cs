5a3c61005282a5c1c86d0ec5c599e162e1f5292c
Thomas Leung <th.leung@mail.utoronto.ca>
2018-05-25 17:17:42 -0400

    Added comments.

--- Assets/Scripts/GenerateStage.cs
+++ Assets/Scripts/GenerateStage.cs
@@ -9,16 +9,17 @@ public class GenerateStage : MonoBehaviour{
      * Used to place collidable surfaces for player interaction.
      * */
 	void Start () {
-        Texture2D platformTexture = Resources.Load("Surface") as Texture2D;
-        ArrayList sprites = new ArrayList();
-        ArrayList platformObjects = new ArrayList();
+        Texture2D platformTexture = Resources.Load("Surface") as Texture2D; //set texture
+        ArrayList sprites = new ArrayList(); //keep track of each sprite created.
+
+        //Currently makes ground for left to right of the screen, 18 blocks)
         for (int i = 0; i < 18; i++)
         {
-            Sprite platformsprite = Sprite.Create(platformTexture, new Rect(55, 42, 100, 100), new Vector2(0, 0));
+            Sprite platformsprite = Sprite.Create(platformTexture, new Rect(0, 0, 100, 100), new Vector2(0, 0));
             GameObject platformobject = new GameObject("Groud");
             SpriteRenderer spriterenderer = platformobject.AddComponent<SpriteRenderer>();
             platformobject.AddComponent<Ground>();
-            platformobject.transform.position = new Vector2(-9 + i, -5);
+            platformobject.transform.position = new Vector2(-9 + i, -5); //Makes it so the position of each block is on a different x coordinate
             spriterenderer.sprite = platformsprite;
             sprites.Add(platformsprite);
         }
