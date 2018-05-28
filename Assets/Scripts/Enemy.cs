98aa61e69c0c3b9d7208afb6996dca5a3507557a
Jason Li <JasonLi3125@gmail.com>
2018-05-27 20:20:14 -0400

    Started enemy class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Character {
+
+	// Use this for initialization
+	public new void Start () {
+        base.Start();
+
+	}
+	
+	// Update is called once per frame
+	public new void Update () {
+        base.Update();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+            
+    }
+}
