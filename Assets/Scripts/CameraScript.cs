eb7c7883b370cc7181fd6bf0edcfa33e920b8d66
Thomas Leung <th.leung@mail.utoronto.ca>
2018-05-28 22:20:05 -0400

    Set a base frame rate (60fps).

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,6 +11,9 @@ public class CameraScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        //Set framerate
+        Application.targetFrameRate = 60;
+
         //Find camera distance
         distance = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0));
         distanceOffset = distance.x * -0.75F;
@@ -30,7 +33,7 @@ public class CameraScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (player.transform.position.x > gameObject.transform.position.x)
+            if (player.transform.position.x > gameObject.transform.position.x)
         {
             gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y,
                 gameObject.transform.position.z);
