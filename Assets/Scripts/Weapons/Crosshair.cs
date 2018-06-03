c2ca358d9dc78a3c8129e00c243d809b21dfee1b
Jason Li <JasonLi3125@gmail.com>
2018-06-03 16:56:17 -0400

    Made mouse into a crosshair

--- Assets/Scripts/Weapons/Crosshair.cs
+++ Assets/Scripts/Weapons/Crosshair.cs
@@ -6,11 +6,16 @@ public class Crosshair : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Get mouse coordinates
+        Vector2 mouseLocation = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+            Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        // Affix crosshair to mouse location (Offset by half downwards, then half to the left)
+        gameObject.transform.position = new Vector2(mouseLocation.x - GetComponent<SpriteRenderer>().bounds.size.x / 2,
+            mouseLocation.y - GetComponent<SpriteRenderer>().bounds.size.y / 2);
 	}
 }
---
+++ Assets/Scripts/Weapons/WeaponOnStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponOnStart : MonoBehaviour {
+    GameObject crosshair;
+
+
+	// Use this for initialization
+	void Start () {
+        Vector2 mouseLocation = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+            Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+        crosshair = new GameObject();
+        Texture2D crosshairTexture = Resources.Load("crosshair") as Texture2D;
+        Sprite crosshairSprite = Sprite.Create(crosshairTexture, new Rect(new Vector2(0, 0),
+            new Vector2(200, 200)), new Vector2(0, 0));
+        SpriteRenderer crosshairRenderer = crosshair.AddComponent<SpriteRenderer>();
+        crosshair.AddComponent<Crosshair>();
+        crosshairRenderer.sprite = crosshairSprite;
+        //Scale it down
+        crosshair.transform.localScale = new Vector3(0.2F, 0.2F, 0.2F);
+
+        //Make cursor invisible
+        Cursor.visible = false;
+
+    }
+	
+}
