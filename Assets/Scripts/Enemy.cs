0047d71c4bdc0241c89774cee7aeaba1876b3496
Jason Li <JasonLi3125@gmail.com>
2018-06-03 21:19:17 -0400

    Put script on enemy and added some test movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,12 +7,17 @@ public class Enemy : Character {
 	// Use this for initialization
 	public new void Start () {
         base.Start();
-
+        velocity = new Vector2(0.1F, 0);
 	}
 	
 	// Update is called once per frame
 	public new void Update () {
         base.Update();
 	}
+
+    private new void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
 
 }
