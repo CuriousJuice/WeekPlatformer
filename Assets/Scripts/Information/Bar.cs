613d1472bdbbf9a396b8fa556429dea4370adef5
Jason Li <JasonLi3125@gmail.com>
2018-06-04 10:26:19 -0400

    Added observer/observable interfaces and started on bar

--- Assets/Scripts/Information/Bar.cs
+++ Assets/Scripts/Information/Bar.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Bar : MonoBehaviour {
+public class Bar : MonoBehaviour, IObservable {
+    List<IObserver> observers;
+    event EventHandler MainHandler;    
+
 
 	// Use this for initialization
 	void Start () {
-		
+        observers = new List<IObserver>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void AddObserver(IObserver o)
+    {
+        if (!observers.Contains(o))
+        {
+            observers.Add(o);
+            MainHandler += o.HandleEvent;
+        }
+    }
+
+    public void DeleteObserver(IObserver o)
+    {
+        if (observers.Contains(o))
+        {
+            observers.Remove(o);
+        }
+    }
+
+    public void NotifyObservers(EventArgs args)
+    {
+        EventHandler thisHandler = MainHandler;
+        if (thisHandler != null)
+        {
+            thisHandler(this, args);
+        }
+    }
 }
---
+++ Assets/Scripts/Observable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public interface IObservable {
+    /// <summary>
+    /// Add an observer
+    /// </summary>
+    /// <param name="o"> The observer to add </param>
+    void AddObserver(IObserver o);
+    /// <summary>
+    /// Delete an observer
+    /// </summary>
+    /// <param name="o"> The observer to delete </param>
+    void DeleteObserver(IObserver o);
+    /// <summary>
+    /// Notify observers about a change
+    /// </summary>
+    void NotifyObservers(EventArgs args);
+    
+}
